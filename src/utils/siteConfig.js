3dde34b5540234fc95ab8ed1523cca6303f3b3aa
Nicole Rose <nrose@lyonscg.com>
2021-01-18 15:01:18 -0500

    master #comment Update site url

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `gifted-kowalevski-393cf8.netlify.app`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://gifted-kowalevski-393cf8.netlify.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
